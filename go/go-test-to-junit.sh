76c02674d740e5047c5188923267f1547e822df4
stanbrub <stanbrubaker@illumon.com>
2024-12-26 12:52:03 -0700

    Added some code coverage

--- go/go-test-to-junit.sh
+++ go/go-test-to-junit.sh
@@ -5,5 +5,12 @@ set -o nounset
 set -o xtrace
 
 XML=${1};
+DO_COVERAGE=${2};
 
-go test -vet=all -v ./... 2>&1 | go-junit-report -set-exit-code -iocopy -out "$XML";
+if [[ "${DO_COVERAGE}" == "true" ]]; then
+  mkdir -p /out/coverage/
+  go test -vet=all -v ./... -coverprofile=c.out 2>&1 | go-junit-report -set-exit-code -iocopy -out "$XML";
+  go tool cover -func=c.out > /out/coverage/go-coverage.tsv;
+else 
+  go test -vet=all -v ./... 2>&1 | go-junit-report -set-exit-code -iocopy -out "$XML";
+fi
---
+++ py/client/run-tests.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+
+DO_COVERAGE=$1
+
+echo "Coverage Enabled = $1" > /out/report/enabled.txt
+
+if [[ "${DO_COVERAGE}" == "true" ]]; then
+  coverage run -m xmlrunner discover tests -v -o /out/report
+  mkdir -p /out/coverage/
+  coverage report > /out/coverage/python-coverage.tsv
+else
+  python -m xmlrunner discover tests -v -o /out/report
+fi
+
